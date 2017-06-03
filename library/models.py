2db094ec6a14c36d1c0a83f8cecb667b4a9d2bbb
Ben Hall <ben@accentdesign.co.uk>
2017-06-03 13:48:24 +0100

    Initial Library models created

--- library/models.py
+++ library/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Platform(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Videogame(models.Model):
+    title = models.CharField(max_length=100)
+    platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
+    available = models.BooleanField(default=1)
