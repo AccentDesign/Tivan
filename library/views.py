e23a45dfb887730c32eb2d58c679c8901f31aaac
Ben Hall <ben@accentdesign.co.uk>
2017-06-03 15:03:22 +0100

    Further setting up of views

--- library/views.py
+++ library/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Videogame
 
 # Create your views here.
+
+
+def index(request):
+    videogames = Videogame.objects.order_by('title')[:10]
+    output = ", ".join(v.title for v in videogames)
+    return HttpResponse(output)
+
+
+def detail(request, videogame_id):
+    return HttpResponse("This is the detail view of the Videogame: %s" % videogame_id)
