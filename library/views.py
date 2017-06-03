605b1aa9f29ef4300c6c6c929126c8ff152cd56f
Ben Hall <ben@accentdesign.co.uk>
2017-06-03 16:11:30 +0100

    Partials added to templates and detail.html done

--- library/views.py
+++ library/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader, RequestContext
+from django.shortcuts import render, get_object_or_404
 from .models import Videogame
 
 # Create your views here.
@@ -8,12 +6,9 @@ from .models import Videogame
 
 def index(request):
     videogames = Videogame.objects.order_by('title')[:10]
-    template = loader.get_template('library/index.html')
-    context = RequestContext(request, {
-        'videogames':videogames
-    })
-    return HttpResponse(template.render(context))
+    return render(request, 'library/index.html', {'videogames': videogames})
 
 
 def detail(request, videogame_id):
-    return HttpResponse("This is the detail view of the Videogame: %s" % videogame_id)
+    videogame = get_object_or_404(Videogame, pk = videogame_id)
+    return render(request, 'library/detail.html', {'videogame': videogame})
