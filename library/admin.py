e0eb139aed03ad67bcc2d2d43f9c3e614cc1d2f8
Ben Hall <ben@accentdesign.co.uk>
2017-06-03 14:19:26 +0100

    Admin Superuser created and library app included in admin

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Platform, Videogame
+
+admin.site.register(Platform)
+admin.site.register(Videogame)
--- library/models.py
+++ library/models.py
@@ -6,8 +6,14 @@ from django.db import models
 class Platform(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
 
 class Videogame(models.Model):
     title = models.CharField(max_length=100)
     platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
     available = models.BooleanField(default=1)
+
+    def __str__(self):
+        return self.title
