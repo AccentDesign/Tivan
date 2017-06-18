caf518f7514b5c52df1fe902a7f21e01a0fc3f7d
Ben Hall <ben@accentdesign.co.uk>
2017-06-18 11:36:56 +0100

    Cover image added to Videogame model, Pillow installed

--- library/admin.py
+++ library/admin.py
@@ -4,5 +4,13 @@ from django.contrib import admin
 
 from .models import Platform, Videogame
 
+
+# set up automated slug creation
+class VideogameAdmin(admin.ModelAdmin):
+    model = Videogame
+    list_display = ('title', 'platform', 'cover', 'available',)
+    prepopulated_fields = {'slug': ('title',)}
+
+
 admin.site.register(Platform)
-admin.site.register(Videogame)
+admin.site.register(Videogame, VideogameAdmin)
--- library/models.py
+++ library/models.py
@@ -13,7 +13,9 @@ class Platform(models.Model):
 class Videogame(models.Model):
     title = models.CharField(max_length=100)
     platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
+    cover = models.ImageField(upload_to = 'cover/', default = 'http://via.placeholder.com/250x350')
     available = models.BooleanField(default=1)
+    slug = models.SlugField(unique=True, default='')
 
     def __str__(self):
         return self.title
