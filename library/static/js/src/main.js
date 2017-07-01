2042751d1fd0c54d4cf88fa011032f3401e08464
Ben Hall <ben@accentdesign.co.uk>
2017-07-01 17:16:48 +0100

    Homepage styled and form toggle functionality added

--- library/static/js/src/main.js
+++ library/static/js/src/main.js
@@ -6,4 +6,19 @@ $(document).ready(function() {
     } else {
         $('body').addClass('no-touch');
     }
+
+    // Homepage Form Switcher
+    $('.toggle--form').click(function(){
+        if(!$(this).hasClass('active')) {
+            $('.toggle--form').removeClass('active');
+            $(this).addClass('active');
+
+            var $wrapper = $(this).data('target');
+            var id = '#';
+            var $wrapper_id = id.concat($wrapper);
+
+            $('.form-wrapper').removeClass('active');
+            $($wrapper_id).addClass('active');
+        }
+    });
 });
