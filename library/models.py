6baeb40a9f968192fcddfb39757b780897a9e0ec
Ben Hall <ben@accentdesign.co.uk>
2017-07-01 16:13:53 +0100

    Adding hooks so profile is created/saved when user is created/saved

--- library/models.py
+++ library/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -36,4 +38,15 @@ class Connection(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     connections = models.ManyToManyField(Connection, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
