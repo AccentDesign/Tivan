da19e55b42cab63c3470e18fcc479c1a742ab720
Ben Hall <ben@accentdesign.co.uk>
2017-07-01 17:59:13 +0100

    Length of Platform name charfield increased

--- library/models.py
+++ library/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 
 class Platform(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=50)
     slug = models.SlugField(unique=True, default='')
 
     def __str__(self):
