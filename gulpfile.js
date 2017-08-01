c9ed1de7bbb509c33267e75c170f61a65645281c
Ben Hall <ben@accentdesign.co.uk>
2017-08-01 09:06:07 +0100

    Improvements made to gulpfile and run-sequence node added

--- gulpfile.js
+++ gulpfile.js
@@ -5,31 +5,37 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var autoprefixer = require('autoprefixer');
 var rename = require('gulp-rename');
+var autoprefixer = require('autoprefixer');
 var postcss = require('gulp-postcss');
 var cssmin = require('gulp-cssmin');
+var runSequence = require('run-sequence');
 
-// Compile Sass
-gulp.task('sass', function() {
-    return gulp.src('library/static/scss/*.scss')
-        .pipe(sass())
-        .pipe(concat('style.css'))
-        .pipe(gulp.dest('library/static'));
-});
+var sassSources = ['library/static/scss/*.scss'];
+var jsSources = ['library/static/js/src/*.js'];
 
-// Concatenate
-gulp.task('scripts', function() {
-    return gulp.src('library/static/js/src/*.js')
+// Concatenate & compress JS
+gulp.task('js-process', function() {
+    return gulp.src(jsSources)
         .pipe(uglify())
         .pipe(concat('main.min.js'))
         .pipe(gulp.dest('library/static/js'));
 });
 
+// Compile Sass
+gulp.task('sass-compile', function() {
+    return gulp.src(sassSources)
+        .pipe(sass())
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('library/static'));
+});
+
 // PostCSS processor
 gulp.task('post-css', function () {
     var processors = [
-        autoprefixer({browsers: ['last 1 version']}),
+        autoprefixer({
+            browsers: ['last 1 version']
+        }),
     ];
     return gulp.src('library/static/style.css')
         .pipe(postcss(processors))
@@ -38,14 +44,16 @@ gulp.task('post-css', function () {
         .pipe(gulp.dest('library/static'))
 });
 
-// Complete CSS processing
-gulp.task('css-process', ['sass', 'post-css']);
+// Process CSS
+gulp.task('css-process', function(callback) {
+    runSequence('sass-compile', 'post-css', callback);
+});
 
-// Watch Files For Changes
+// Watch files for changes
 gulp.task('watch', function() {
-    gulp.watch('library/static/scss/*.scss', ['css-process']);
-    gulp.watch('library/static/js/*.js', ['scripts']);
+	gulp.watch(jsSources, ['js-process']);
+	gulp.watch(sassSources, ['css-process']);
 });
 
 // Default Task
-gulp.task('default', ['sass', 'scripts', 'post-css', 'watch']);
+gulp.task('default', ['js-process', 'css-process', 'watch']);
