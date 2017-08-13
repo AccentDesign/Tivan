fdaea27d6004190643fc6859302ab01a0a9773f6
Ben Hall <ben@accentdesign.co.uk>
2017-08-13 11:21:04 +0100

    Homepage styled to v1

--- library/static/js/src/main.js
+++ library/static/js/src/main.js
@@ -20,12 +20,32 @@ $(document).ready(function() {
             $('.toggle--form').removeClass('active');
             $(this).addClass('active');
 
-            var $wrapper = $(this).data('target');
+            var wrapper = $(this).data('target');
             var id = '#';
-            var $wrapper_id = id.concat($wrapper);
+            var wrapper_id = id.concat(wrapper);
+            var str = '_text';
+            var text_id = wrapper_id.concat(str);
+
+            if (wrapper == 'register') {
+                $('.home--login .gradient').removeClass('gradient--secondary');
+                $('.home--login .gradient').addClass('gradient--primary');
+            } else if (wrapper == 'login') {
+                $('.home--login .gradient').removeClass('gradient--primary');
+                $('.home--login .gradient').addClass('gradient--secondary');
+            }
 
             $('.form-wrapper').removeClass('active');
-            $($wrapper_id).addClass('active');
+            $(wrapper_id).addClass('active');
+
+            $('.text-wrapper').removeClass('active');
+            $(text_id).addClass('active');
         }
+    });
+
+    // Scroll to Register
+    $('#register_link').click(function(){
+        $('html, body').animate({
+            'scrollTop': $('.home--login').offset().top
+        }, 500);
     });
 });
