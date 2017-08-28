95fd9a264429c610e25f83a15cd7cda6a5e49268
Ben Hall <ben@accentdesign.co.uk>
2017-08-28 13:39:35 +0100

    Moved IGDB API script

--- tivanapp/views.py
+++ tivanapp/views.py
@@ -1,31 +1,85 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.template.loader import get_template
+from django.core.mail import EmailMessage
+from django.template import Context
 from django.http import Http404
-from .models import MediaItem
-from .forms import MediaItemForm, EditUserForm, UserSearchForm
+from registration.forms import RegistrationForm
+from .forms import ContactForm, MediaItemForm, EditUserForm
+from library.models import MediaItem
 
 
 # Create your views here.
 
 
-def index(request, initial=None):
+def home(request):
+    reg_form_class = RegistrationForm
+    auth_form_class = AuthenticationForm
+    return render(request, 'home.html', {'reg_form': reg_form_class, 'auth_form': auth_form_class,})
+
+
+def contact(request):
+    form_class = ContactForm
+    if request.method == 'POST':
+        form = form_class(data=request.POST)
+
+        if form.is_valid():
+            contact_name = form.cleaned_data['contact_name']
+            contact_email = form.cleaned_data['contact_email']
+            form_content = form.cleaned_data['content']
+
+            # email the profile with the contact info
+            template = get_template('contact_template.txt')
+
+            context = Context({
+                'contact_name': contact_name,
+                'contact_email': contact_email,
+                'form_content': form_content
+            })
+            content = template.render(context)
+
+            email = EmailMessage(
+                'New contact form submission',
+                content,
+                'Contact from Tivan',
+                ['ben@accentdesign.co.uk'],
+                headers={'Reply-To': contact_email}
+            )
+            email.send()
+
+            messages.success(request, 'Message Sent.')
+            return redirect('home')
+
+    return render(request, 'contact.html', {'form': form_class})
+
+
+def welcome(request):
+    return render(request, 'welcome.html')
+
+
+def connection(request):
+    return render(request, 'connection.html')
+
+
+@login_required
+def library(request, initial=None):
     if initial:
         items = MediaItem.objects.filter(title__istartswith=initial).order_by('title')
     else:
         items = MediaItem.objects.order_by('title')
 
     users = User.objects.order_by('username')
-    form_class = UserSearchForm
     return render(request, 'library/index.html', {
         'items': items,
         'initial': initial,
         'users': users,
-        'form': form_class
     })
 
 
+@login_required
 def detail(request, slug):
     item = MediaItem.objects.get(slug=slug)
     return render(request, 'library/detail.html', {'item': item})
@@ -49,7 +103,7 @@ def edit(request, slug):
             form.save()
 
             messages.success(request, 'Item Updated.')
-            return redirect('library:detail', slug=item.slug)
+            return redirect('detail', slug=item.slug)
     # otherwise just create the form
     else:
         form = form_class(instance=item)
@@ -58,27 +112,36 @@ def edit(request, slug):
 
 
 @login_required
-def add(request):
+def collection(request):
+    items = MediaItem.objects.order_by('title')
+    users = User.objects.order_by('username')
+
     form_class = MediaItemForm
-    # if we're coming from a submitted form, do this if request.method == 'POST':
-    # grab the data from the submitted form and apply to # the form
+    # if we're coming from a submitted form, do this
     if request.method == 'POST':
+        # grab the data from the submitted form and apply to # the form
         form = form_class(request.POST)
         if form.is_valid():
             # create an instance but do not save yet
             item = form.save(commit=False)
             # set the additional details
             item.user = request.user
+
             # save the object
             item.save()
 
             messages.success(request, 'Item Added.')
             # redirect to our newly created thing
-            return redirect('library:detail', slug=item.slug)
+            return redirect('your_collection', slug=item.slug)
     # otherwise just create the form
     else:
         form = form_class()
-        return render(request, 'library/add.html', {'form': form})
+
+    return render(request, 'library/your_collection.html', {
+        'items': items,
+        'users': users,
+        'form': form,
+    })
 
 
 @login_required
@@ -94,4 +157,3 @@ def edit_profile(request):
     else:
         form = form_class(instance=user)
         return render(request, 'library/edit_profile.html', {'user': user, 'form': form})
-
